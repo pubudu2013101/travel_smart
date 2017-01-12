e675cd0836fc8eb3d77164fa65dec01576286dce
Lahiru Madhusankha <madhusankhalahiru@gmail.com>
2017-01-12 20:31:47 +0530

    how to play carousel added

--- ipad/how_to_play.php
+++ ipad/how_to_play.php
@@ -9,42 +9,10 @@
 
         <?php include 'jquery_loading.php'; ?>
 
-        <script type="text/javascript">
-            $(document).ready(function () {
-                $('#buttons').jsShare({maxwidth: 370});
-            });
-        </script>
-
-        <script type="text/javascript" src="https://cdn.jsdelivr.net/jquery.jssocials/1.4.0/jssocials.min.js"></script>
-
-        <link type="text/css" rel="stylesheet" href="https://cdn.jsdelivr.net/jquery.jssocials/1.4.0/jssocials.css"/>
-
-        <link type="text/css" rel="stylesheet"
-              href="https://cdn.jsdelivr.net/jquery.jssocials/1.4.0/jssocials-theme-flat.css"/>
-        <link type="text/css" rel="stylesheet"
-              href="https://cdn.jsdelivr.net/jquery.jssocials/1.4.0/jssocials-theme-classic.css"/>
-        <link type="text/css" rel="stylesheet"
-              href="https://cdn.jsdelivr.net/jquery.jssocials/1.4.0/jssocials-theme-minima.css"/>
-        <link type="text/css" rel="stylesheet"
-              href="https://cdn.jsdelivr.net/jquery.jssocials/1.4.0/jssocials-theme-plain.css"/>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-
         <style>
 
             #mypanel {
                 background: #396E9B;
-            }
-
-            .carousel-inner > .item > img,
-            .carousel-inner > .item > a > img {
-
-                width: 70%;
-                margin-left: 15%;
-                margin-top: 10%;
             }
 
             [data-role="header"] {
@@ -67,9 +35,9 @@
 
             }
 
-            h1, h2 {
+            h1 {
                 color: #396E9B;
-                margin-left: 2%;
+
             }
 
             #u_image {
@@ -89,98 +57,130 @@
                 margin-top: 6%;
             }
 
-            .ui-block-a {
-                border-right: 1px solid;
-                text-align: left;
-                height: 100%;
+            #footer {
+                background: #ffffff;
             }
 
-            .ui-block-a {
-                border-right: 1px solid;
-                text-align: match-parent;
-                height: 100%;
+            img {
+                border-radius: 5px;
             }
 
-            h3 {
-                margin-left: 6%;
-                color: #396E9B;
-            }
-
-            #contact {
+            .poster-main {
+                /*position: relative;*/
+                /*margin-top: 30%;*/
+                margin: 80%;
+                max-width: 100px;
                 text-align: center;
             }
 
-            #address {
-                text-align: left;
-                margin-left: 6%;
+            .poster-main .poster-list .poster-item {
+                position: absolute;
+                left: 0;
+                top: 0;
             }
 
-            p {
-                margin: 6%;
+            .poster-main .poster-btn {
+                position: absolute;
+                top: 0;
+                cursor: pointer;
             }
 
-            body{
-                background-image: image(../images/game.jpg)
+            .poster-main .poster-prev-btn {
+                left: 0;
+                background: url("../images/btn_l.png") no-repeat center center;
             }
 
-            #share {
-                margin-left: 6%;
-                color: #FFFFFF;
+            .poster-main .poster-next-btn {
+
+                background: url("../images/btn_r.png") no-repeat center center;
             }
 
-            .jssocials-share-link {
-                border-radius: 0%;
+            .poster-list li{
+                margin-left:5% !important;
+                margin-top: 5% !important;
             }
-
+            .poster-btn {
+                margin-left: 5% !important;
+                margin-top: -10% !important;
+            }
 
         </style>
+
+        <script src="../http://code.jquery.com/jquery-1.12.4.min.js"></script>
+        <script src="../js/Carousel.js"></script>
+        <script>
+            $(function () {
+                Carousel.init($("#carousel"));
+                $("#carousel").init();
+            });
+        </script>
+        <script type="text/javascript">
+
+            var _gaq = _gaq || [];
+            _gaq.push(['_setAccount', 'UA-36251023-1']);
+            _gaq.push(['_setDomainName', 'jqueryscript.net']);
+            _gaq.push(['_trackPageview']);
+
+            (function () {
+                var ga = document.createElement('script');
+                ga.type = 'text/javascript';
+                ga.async = true;
+                ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+                var s = document.getElementsByTagName('script')[0];
+                s.parentNode.insertBefore(ga, s);
+            })();
+
+        </script>
 
     </head>
 
     <body>
 
+    <div data-role="header" data-position="fixed" data-tap-toggle="false" data-theme="a">
+        <a href="#mypanel"
+           class="ui-btn ui-shadow ui-corner-all ui-icon-bars ui-btn-icon-notext ui-nodisc-icon ui-alt-icon"
+           id="navbtn">Search</a>
+        <h1>How To Play</h1>
+
+    </div>
+
+
     <div data-role="main" class="ui-content">
 
-        <div data-role="header" data-position="fixed" data-tap-toggle="false" data-theme="a">
-            <a href="#mypanel"
-               class="ui-btn ui-shadow ui-corner-all ui-icon-bars ui-btn-icon-notext ui-nodisc-icon ui-alt-icon"
-               id="navbtn">Search</a>
-            <h1>How to play</h1>
-
-            <div id="myCarousel" class="carousel slide" data-ride="carousel">
-                <!-- Indicators -->
-                <ol class="carousel-indicators">
-                    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-                    <li data-target="#myCarousel" data-slide-to="1"></li>
-                    <li data-target="#myCarousel" data-slide-to="2"></li>
-                </ol>
-
-                <!-- Wrapper for slides -->
-                <div class="carousel-inner" role="listbox">
-                    <div class="item active">
-                        <img src="../../travel_smart/images/image2.jpg" alt="Chania">
-                    </div>
-
-                    <div class="item">
-                        <img src="../../travel_smart/images/image4.jpg" alt="Chania">
-                    </div>
-
-                    <div class="item">
-                        <img src="../../travel_smart/images/image3.jpg" alt="Flower">
-                    </div>
-                </div>
-
-                <!-- Left and right controls -->
-                <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
-                    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-                    <span class="sr-only">Previous</span>
-                </a>
-                <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
-                    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-                    <span class="sr-only">Next</span>
-                </a>
-            </div>
+        <div class="poster-main" id="carousel" data-setting='{
+							"width":900,
+							"height":900,
+							"posterWidth":700,
+							"posterHeight":1000,
+							"scale":0.8,
+							"speed":1000,
+							"autoPlay":true,
+							"delay":3000,
+							"verticalAlign":"middle"
+							}'>
+            <div class="poster-btn poster-prev-btn"></div>
+            <ul class="poster-list">
+                <li class="poster-item"><a href="#"><img src="https://unsplash.it/600/500?image=237" alt="" width="100%"/></a>
+                </li>
+                <li class="poster-item"><a href="#"><img src="https://unsplash.it/600/500?image=239" alt="" width="100%"/></a>
+                </li>
+                <li class="poster-item"><a href="#"><img src="https://unsplash.it/600/500?image=250" alt="" width="100%"/></a>
+                </li>
+                <li class="poster-item"><a href="#"><img src="https://unsplash.it/600/500?image=223" alt="" width="100%"/></a>
+                </li>
+                <li class="poster-item"><a href="#"><img src="https://unsplash.it/600/500?image=195" alt="" width="100%"/></a>
+                </li>
+            </ul>
+            <div class="poster-btn poster-next-btn"></div>
         </div>
+
+    </div>
+
+    <div data-role="footer" data-position="fixed" data-tap-toggle="false" id="footer">
+
+        <h1>DroinNTec Solutions</h1>
+
+    </div>
 
     </body>
 
---
+++ ipad/upcoming_events.php
@@ -0,0 +1,138 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<div data-role="page" data-ajax="true">
+
+    <?php include 'panel.php'; ?>
+
+    <head>
+
+        <?php include 'jquery_loading.php'; ?>
+
+        <style>
+
+            #mypanel {
+                background: #396E9B;
+            }
+
+            [data-role="header"] {
+                background: #ffffff;
+            }
+
+            #btn1, #btn2, #btn3, #btn4, #btn5, #btn6, #btn7, #btn8, #btn9 {
+                background: #ffffff;
+                text-align: left;
+                padding-left: 25%;
+                /*color:#396E9B;*/
+            }
+
+            .ui-content {
+                background: #ffffff;
+            }
+
+            #navbtn {
+                margin-left: 2%;
+
+            }
+
+            h1 {
+                color: #396E9B;
+
+            }
+
+            #u_image {
+                margin-left: 30%;
+                width: 40%;
+                height: 40%;
+            }
+
+            #name {
+                color: #ffffff;
+                font-weight: normal;
+
+            }
+
+            hr {
+                margin-bottom: 6%;
+                margin-top: 6%;
+            }
+
+            #footer {
+                background: #ffffff;
+            }
+
+            #place_image{
+                margin: 10px;
+                margin-top: 15px;
+            }
+
+        </style>
+
+        <script>
+
+            var my_email = location.search.split('user_email=')[1];
+
+            my_email = 'pubudujayasanka@gmail.com';
+
+            var arr = {user_email: my_email};
+
+
+            $(function () {
+
+                if (my_email == '') {
+                    alert("No place selected");
+                }
+                else {
+
+                    $.ajax({
+                        url: 'http://travelsmartwebapp.azurewebsites.net/ux_ui_backend/index.php/event_controller/get_events',
+                        type: 'POST',
+                        data: JSON.stringify(arr),
+                        contentType: 'application/json; charset=utf-8',
+                        dataType: 'json',
+                        async: false,
+                        success: function (data) {
+
+                            $.each(data, function(i, item) {
+
+                                $('ul').append('<li><a href=""><img src=" ' +  item.event_images  + '" id="place_image" width="200px">' + item.event_header + '<p><br>' + item.event_details + '<br>' + item.event_date + '</p></a></li>');
+                            });
+
+                            $('#output').listview("refresh");
+
+                        }
+                    });
+                }
+            });
+
+        </script>
+
+    </head>
+
+    <body>
+
+    <div data-role="header" data-position="fixed" data-tap-toggle="false" data-theme="a">
+        <a href="#mypanel"
+           class="ui-btn ui-shadow ui-corner-all ui-icon-bars ui-btn-icon-notext ui-nodisc-icon ui-alt-icon"
+           id="navbtn">Search</a>
+        <h1>Upcoming Events</h1>
+
+    </div>
+
+
+    <div data-role="main" class="ui-content">
+
+        <ul data-role="listview" data-filter="false" id="output" data-theme="a" data-inset="true"></ul>
+
+    </div>
+
+    <div data-role="footer" data-position="fixed" data-tap-toggle="false" id="footer">
+
+        <h1>DroinNTec Solutions</h1>
+
+    </div>
+
+    </body>
+
+</div>
+</html>
